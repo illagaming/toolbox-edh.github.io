d4850ff53e8473df475fdc68b6aed45aa958b1eb
paul-arnaud78 <arnaudpaul15@gmail.com>
2021-05-31 15:58:28 +0200

    Finition de la page informatique

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 function recupId(clicked_id)
-{       
+{     
+          
      var idinfo = document.getElementById(clicked_id);
-     alert(idinfo);
 
+     var tools = document.getElementById('tools');  
+     var tuto = document.getElementById('tuto');
+     var good = document.getElementById('good-to-do');
+
+     tools.style.display = 'none';
+     tuto.style.display = 'none';
+     good.style.display = 'none';
+     
      if (idinfo.style.display == 'none')
      {
           idinfo.style.display = 'block';       
      }
-          else
+     else
      {
           idinfo.style.display = 'none';
      }     
